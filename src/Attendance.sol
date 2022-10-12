07650c6a9e8fe3af3c4c40dff58e4a5d5191d37e
benchpre <edwardcho140@gmail.com>
2022-10-12 14:43:36 +0900

    feat: attendance까지 compile 되도록 수정

--- src/Attendance.sol
+++ src/Attendance.sol
@@ -2,19 +2,19 @@ pragma solidity ^0.8.15;
 
 import "./interface/IAttendance.sol";
 
-// contract Attendance is IAttendance {
-// mapping(address => mapping(uint256 => AttendanceState)) public attendanceStatus;
+contract Attendance is IAttendance {
+    mapping(address => mapping(uint256 => string)) public attendanceStatus;
 
-//     function getAttendance(address member, uint256 date) public view returns (AttendanceState) {
-//         return attendanceStatus[member][date];
-//     }
+    function getAttendance(address member, uint256 date) external view returns (string memory attendance) {
+        return attendanceStatus[member][date];
+    }
 
-//     function setAttendance(
-//         address member,
-//         string state,
-//         uint256 date
-//     ) public {
-//         require(attendanceStatus[member][date] == 0, "State is already written.");
-//         attendanceStatus[member][date] = AttendanceState(state);
-//     }
-// }
+    function setAttendance(
+        address member,
+        string memory state,
+        uint256 date
+    ) external {
+        require(bytes(attendanceStatus[member][date]).length > 0, "State is already written.");
+        attendanceStatus[member][date] = state;
+    }
+}
--- src/interface/IAttendance.sol
+++ src/interface/IAttendance.sol
@@ -1,13 +1,7 @@
 pragma solidity ^0.8.15;
 
 interface IAttendance {
-    // enum AttendanceState {
-    //     Absent,
-    //     Late,
-    //     Attend
-    // }
-
-    // function getAttendance(address member, uint256 date) public view returns (attendanceStatus);
+    function getAttendance(address member, uint256 date) external view returns (string memory attendance);
 
     function setAttendance(
         address member,
